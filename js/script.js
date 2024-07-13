d9fedf4026ab41b45ce31e997c60f5de6a132936
viniciusbarbosa987 <162506194+viniciusbarbosa987@users.noreply.github.com>
2024-07-12 22:24:48 -0300

    Desenvolvendo função a vírgula da calculadora

--- js/script.js
+++ js/script.js
@@ -121,8 +121,30 @@ function realizarOperação(expressão) {
 function identificarExpressao(botoesClicados) {
     let formadorDeNumero = ''
     let expressão = []
+    let numeroDeCasasAntesDaVirgula = 0
+    let numeroDeCasasDepoisDaVirgula = 0
+    
+    //Verifica se há números decimais + Identifica quantas casas existem antes e após a vírgula no número decimal
+    if (botoesClicados.indexOf(',') != -1) {
+        let pos = botoesClicados.indexOf(',')
+        
+        //Identificar quantas casas há antes da vírgula
+        while (pos >= 0 && verificarTipo(botoesClicados[pos-1]) == 'numero') {
+            numeroDeCasasAntesDaVirgula ++
+            pos --
+        }
 
-    //Análise da expressão / identificação dos números e dos operadores
+        pos = botoesClicados.indexOf(',')
+
+        //Identificar quantas casas há após a vírgula
+        while (pos <= (botoesClicados.length - 1) && verificarTipo(botoesClicados[pos+1]) == 'numero') {
+            numeroDeCasasDepoisDaVirgula ++
+            pos ++
+        }
+ 
+    }
+    
+    //Identificação dos números e dos operadores
     for (let pos in botoesClicados) {
         if (typeof botoesClicados[pos] == 'number') {
             formadorDeNumero += String(botoesClicados[pos])
@@ -131,7 +153,32 @@ function identificarExpressao(botoesClicados) {
             expressão.push(botoesClicados[pos])
             formadorDeNumero = ''    
         }
-    }    
+    }   
+    
+    if (expressão.indexOf(',') != -1) {
+        function verificarSeONumeroDecimalFoiTotalmenteEscrito() {
+            let pos = expressão.indexOf(',')
+            if (pos == (expressão.length - 1)) {
+                return false
+            } else {
+                while (pos + 1 <= (expressão.length - 1)) {
+                    if (verificarTipo(expressão[pos+1]) == 'numero') {
+                        pos++
+                    } else {
+                        return true
+                    }
+                }
+            }
+            
+        }
+        
+        if (verificarSeONumeroDecimalFoiTotalmenteEscrito() == true) {
+            expressão[expressão.indexOf(',') - 1] = expressão[expressão.indexOf(',') - 1] + (expressão[expressão.indexOf(',') + 1])/(10**(numeroDeCasasDepoisDaVirgula))
+            //Falta remover a vírgula e o número após a vírgula
+            //Acho que vai ser desnecessário a váriavel numeroDeCasaAntesDaVírgula
+            //Tenho que fazer mais comentários nesse trecho do código
+        }
+    }
     
     //Retorna o array da expressão quando o botão igual é clicado
     if (expressão[expressão.length - 1] == '=') {
